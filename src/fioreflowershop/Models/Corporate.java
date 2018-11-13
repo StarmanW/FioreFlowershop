dcf4744ccee88dc9227fa21032342b507bdba688
StarmanW <swkf3011@hotmail.com>
2018-11-13 20:10:44 +0800

    Minor update to Corporate model

--- src/fioreflowershop/Models/Corporate.java
+++ src/fioreflowershop/Models/Corporate.java
@@ -10,17 +10,19 @@ public class Corporate extends Customer {
     // Private properties
     private String corporateId;
     private String corporateName;
-
+    private double creditLimit;
+    
     // Default constructor
     public Corporate() {
-        this("CO0001", "Starman Ltd.", "No.45, Jalan Wangsa Maju, Petagas, Wilayah Persekutuan, Kuala Lumpur, Malaysia", "088654755");
+        this("CO0001", "Starman Ltd.", "No.45, Jalan Wangsa Maju, Petagas, Wilayah Persekutuan, Kuala Lumpur, Malaysia", "088654755", 1000.00);
     }
 
     // Parameterized constructor
-    public Corporate(String corporateId, String corporateName, String address, String contactNo) {
+    public Corporate(String corporateId, String corporateName, String address, String contactNo, double creditLimit) {
         super(address, contactNo);
         this.corporateId = corporateId;
         this.corporateName = corporateName;
+        this.creditLimit = creditLimit;
     }
 
     // Getters
@@ -32,6 +34,10 @@ public class Corporate extends Customer {
         return corporateName;
     }
 
+    public double getCreditLimit() {
+        return creditLimit;
+    }
+
     // Setters
     public void setCorporateId(String corporateId) {
         this.corporateId = corporateId;
@@ -39,5 +45,9 @@ public class Corporate extends Customer {
 
     public void setCorporateName(String corporateName) {
         this.corporateName = corporateName;
+    }
+
+    public void setCreditLimit(double creditLimit) {
+        this.creditLimit = creditLimit;
     }
 }
