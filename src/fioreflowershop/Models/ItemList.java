d69256b64c6782040e17097fbc65215069d940d7
chongjh <chongjh-sa16@student.tarc.edu.my>
2018-11-13 19:57:30 +0800

    Added ItemList class

---
+++ src/fioreflowershop/Models/ItemList.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fioreflowershop.Models;
+
+/**
+ *
+ * @author ChongJH
+ * @param <T>
+ */
+public class ItemList<T> extends ArrayList<Object>{        
+    public int checkStock() {
+        return 0;
+    }
+    
+    public Object sortByType(Object T) {
+        return null;
+    }
+}
