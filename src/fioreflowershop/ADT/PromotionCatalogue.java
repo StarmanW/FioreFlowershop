83ddede5db7497770243614cf7666f42ff15d69e
chongjh <chongjh-sa16@student.tarc.edu.my>
2018-11-29 14:28:31 +0800

    Moved PromotionCatalogue to ADT folder

--- src/fioreflowershop/ADT/PromotionCatalogue.java
+++ src/fioreflowershop/ADT/PromotionCatalogue.java
@@ -3,7 +3,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package fioreflowershop.Models;
+package fioreflowershop.ADT;
+
+import fioreflowershop.Models.Catalogue;
+import fioreflowershop.Models.Product;
 
 /**
  *
