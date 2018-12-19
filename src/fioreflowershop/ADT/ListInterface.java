08423a51047a3b7804499c86424015f4b0c3b840
chongjh <chongjh-sa16@student.tarc.edu.my>
2018-12-19 21:49:43 +0800

    Removed unused method

--- src/fioreflowershop/ADT/ListInterface.java
+++ src/fioreflowershop/ADT/ListInterface.java
@@ -11,11 +11,22 @@ package fioreflowershop.ADT;
  * @param <T>
  */
 public interface ListInterface<T extends Comparable<? super T>> {
+    //add an item to linked list
     public boolean add(T item);
+    
+    //remove an item of given idex from linked list
     public boolean remove(int index);
-    public boolean replace(int index, T item);
+    
+    //returns the item of given index
     public T get(int index);
+    
+    //checks if the given item is in the linked list. Returns it's index if it's
+    //in the linked list
     public int contains(T item);
+    
+    //returns the current size of the linked list
     public int size();
+    
+    //checks if the linked list is empty
     public boolean isEmpty();
 }
